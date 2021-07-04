d0f8333ce1b8c403fc1c63a197c870ef32757a21
Cashmay0r <aidanenseleit98@gmail.com>
2021-07-04 17:13:02 +1000

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const app = express();
+const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(__dirname + '/public/css/'));
+app.use(express.static(__dirname + '/public/js/'));
+app.use(express.static(__dirname + '/public/images/'));
+
+app.get('/', (req, res) => {
+	res.sendFile(__dirname + '/public/html/index.html');
+});
+app.listen(port, () => {
+	console.log(`Server Listening at http://localhost:${port}`);
+});
